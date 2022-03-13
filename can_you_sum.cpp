a0664259bc0ed0dd0a8e0b74035d7bc36026f761
Iftekhar Md. Shishir <84434133+Ciftekhar@users.noreply.github.com>
2022-03-13 15:19:21 +0600

    Update can_you_sum.cpp

--- can_you_sum.cpp
+++ can_you_sum.cpp
@@ -1,5 +1,55 @@
-1. Time complexity O(huge) | Brute force approch
-   Is is possible to generate the sum using the numbers of the array (as many times as possible)?
+Q. Is is possible to generate the sum using the numbers of the array (as many times as possible)?
+=> input
+3
+2 7
+3 2
+4 7
+3 4 5 7
+1 7
+2
+=> output 
+possible
+possible
+impossible
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+1. Time complexity O(n^m) | Space complexity O(m) | Best approch
+   
+ll canSum(ll targetSum, set<ll> a, vector<ll> &mm)
+{
+        if(mm[targetSum] != -1) return mm[targetSum];
+        if(targetSum == 0) return 1;
+        if(targetSum < 0) return 0;
+
+        for(auto x:a){
+            ll remainder = targetSum - x;
+            if(canSum(remainder,a,mm) == 1){
+                mm[targetSum] = 1;
+                return mm[targetSum];
+            }
+        }
+        mm[targetSum] = 0;
+        return mm[targetSum];
+}
+string solve()
+{
+        ll n = vin();
+        ll m = vin();
+        set<ll> a;
+        vector<ll> mm(1001,-1);
+        for(int i=0; i<n; i++) {
+            ll x = vin();
+            a.insert(x);
+        }
+        ll res = canSum(m,a,mm);
+        if(!res) return "impossible";
+        return "possible";
+}
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+2. Time complexity O(n^m) | Space complexity O(m) | Brute force approch
 
 bool canSum(ll targetSum, vector<ll> a)
 {
@@ -26,15 +76,4 @@ string solve()
         return "possible";
 }
 
-=> input
-3
-2 7
-3 2
-4 7
-3 4 5 7
-1 7
-2
-=> output 
-possible
-possible
-impossible
+
