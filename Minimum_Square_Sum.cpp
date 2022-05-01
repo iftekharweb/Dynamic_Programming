d9137a04e4306cf1407541ec75ba22918e5f626e
Iftekhar Md. Shishir <84434133+iftekharweb@users.noreply.github.com>
2022-05-01 12:49:43 +0600

    Create Minimum_Square_Sum.cpp

---
+++ Minimum_Square_Sum.cpp
@@ -0,0 +1,28 @@
+1. Time COmplexity : O(n) | Memorization Apprroch (Top-Bottom)
+
+vector<ll> dp(N,MOD);
+ll MinSquare(ll n) {
+        if(dp[n] != MOD) return dp[n];
+        if(n==0 || n==1 || n==2 || n==3) return n;
+        for(int i=1; i*i<=n; i++) {
+            dp[n]  = min(MinSquare(n-i*i)+1, dp[n]);
+        }
+        return dp[n];
+}
+
+
+2. Time Complexity : O(n)  |  Iterative Approch (Bottom-Top)
+
+vector<ll> dp(N,MOD);
+ll MinSquare(ll n) {
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        for(int i=0; i*i<=n; i++) {
+            for(int j=0; i*i+j<=n; j++) {
+                dp[i*i+j] = min(dp[i*i+j], 1+dp[j]);
+            }
+        }
+        return dp[n];
+}
